26fd7d1aa6455cf3e723fefc67fc0564014916c9
kasesalum <kasesalum@gmail.com>
2023-02-26 21:50:09 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+function yesnoCheck(that) {
+  if (that.value == "pangalink") {
+    document.getElementById("ifYes").style.display = "block";
+  } else {
+    document.getElementById("ifYes").style.display = "none";
+  }
+}
